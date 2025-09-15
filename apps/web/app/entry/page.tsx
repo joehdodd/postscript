c78cd1f60e49774fc47e52a466bc56f22dc8c4b1
joehdodd <joehdodd@gmail.com>
2025-09-15 16:13:39 -0400

    WIP. Updates FE logic around tokens.

--- apps/web/app/entry/page.tsx
+++ apps/web/app/entry/page.tsx
@@ -1,7 +1,19 @@
 import { Card } from "@repo/ui/card";
 import EntryForm from "../components/EntryForm";
+import { redirect } from 'next/navigation';
+import { auth } from '../actions/auth';
+import { getTokenFromCookieOrSearchParams } from "../utils/auth-util";
 
-export default async function Entry() {
+type EntryPageProps = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export default async function Entry({ searchParams }: EntryPageProps) {
+  const token = await getTokenFromCookieOrSearchParams(new URLSearchParams(searchParams as Record<string, string>));
+  if (!token) {
+    redirect('/');
+  }
+  await auth(token);
   return (
     <Card className="max-w-md w-full">
       <h2 className="text-xl text-slate-600 dark:text-slate-200 font-bold">Add a new entry</h2>
---
+++ apps/web/app/utils/auth-util.ts
@@ -0,0 +1,12 @@
+import { cookies } from 'next/headers';
+
+export async function getTokenFromCookieOrSearchParams(searchParams: URLSearchParams) {
+  // Try to get token from cookies first
+  const cookieStore = cookies();
+  const cookieToken = (await cookieStore).get('token')?.value;
+  if (cookieToken) return cookieToken;
+
+  // Fallback: get token from search params
+  const urlToken = searchParams.get('token');
+  return urlToken;
+}
